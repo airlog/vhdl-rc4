24c2bee561775aea4bcbaa2b0cefdaf0358ab1f6
robin92 <robin92pl@gmail.com>
2014-05-30 15:11:45 +0200

    Przenoszenie pamieci na zewnatrz.

--- src/rc4_crypto.vhd
+++ src/rc4_crypto.vhd
@@ -7,65 +7,43 @@ entity rc4_crypto is
 		width: integer := 8
 	);
 	port (
-		input: in std_logic_vector((width - 1) downto 0);
-		go: in std_logic;
+		enc_input: in std_logic_vector((width - 1) downto 0);	-- bajt do zaszyfrowania/deszyfrowania
+		perm_input: in std_logic_vector((width - 1) downto 0);	-- wejscie wartosci z pamieci
+		go: in std_logic;										-- dzialac/nie dzialac
 		clk: in std_logic;
-		output: out std_logic_vector((width - 1) downto 0);
-		rdy: out std_logic
+		enc_output: out std_logic_vector((width - 1) downto 0);	-- zaszyfrowany/deszyfrowany bajt
+		perm_ctrl: out std_logic;								-- zapis/odczyt z pamieci
+		perm_index: out std_logic_vector((width - 1) downto 0);	-- indeks bajtu w pamieci
+		perm_output: out std_logic_vector((width - 1) downto 0);-- perm_ctrl=1 => zapisz te wartosc
+		rdy: out std_logic										-- wartosc na enc_output jest poprawna
 	);
 end rc4_crypto;
 
 architecture Behavioral of rc4_crypto is
-	component memory
-		generic (
-			width: integer := 8;	-- ilosc bitow adresów
-			size: integer := 256	-- rozmiar pamieci w bajtach
-		);
-		port (
-			SET: in STD_LOGIC;												-- tryb pracy
-			CLK: in STD_LOGIC;												-- zegar
-			INDEX: in STD_LOGIC_VECTOR ((width - 1) downto 0);		-- indeks elementu tablicy
-			INVALUE: in STD_LOGIC_VECTOR ((width - 1) downto 0);	-- wartoœæ wejœciowa
-			OUTVALUE: out STD_LOGIC_VECTOR ((width - 1) downto 0)	-- wartoœæ wyjœciowa
-		);
-	end component;
-	
-	signal block_set : std_logic := '0';
-	signal block_index : std_logic_vector((width - 1) downto 0);
-	signal block_inval : std_logic_vector((width - 1) downto 0);
-	signal block_outval : std_logic_vector((width - 1) downto 0);
-	
-	constant PERM_SIZE : integer := 256; 
-	
 	type rc4_crypto_state is (
-			WHILE_0_TEST, WHILE_GO_TEST, WHILE_GO_RET,
+			WHILE_GO_TEST, WHILE_GO_RET,
 			MAIN_BODY, MAIN_BODY_SET_J, MAIN_BODY_SWAP_SI, MAIN_BODY_SWAP_SJ,
 			MAIN_BODY_PREPARE_OUTPUT, MAIN_BODY_OUTPUT,
 			WHILE_0_RET
 		);
 	subtype rc4int is integer range 0 to 255;
-	type rc4_array is array (0 to (PERM_SIZE - 1)) of rc4int;
-		
-	shared variable cstate : rc4_crypto_state := WHILE_0_TEST;
+	
+	shared variable cstate : rc4_crypto_state := WHILE_GO_TEST;
 	shared variable i, j, tmp, si, sj, sm : rc4int := 0;
 begin
-	perm : memory
-		generic map(width => width, size => PERM_SIZE)
-		port map(block_set, clk, block_index, block_inval, block_outval);
-
 	process (clk)
+		variable clk_ctr : integer := 0;
 	begin
 		if rising_edge(clk) then
 			rdy <= '0';
+			perm_ctrl <= '0';
 			case cstate is
-				when WHILE_0_TEST =>
-					cstate := WHILE_GO_TEST;
-				
 				when WHILE_GO_TEST =>
 					if go = '1' then
+						enc_output <= "11000000";
 						cstate := MAIN_BODY;
 					else
-						cstate := WHILE_GO_RET;
+						enc_output <= "00000000";
 					end if;
 					
 				when WHILE_GO_RET =>
@@ -73,49 +51,49 @@ begin
 				
 				when MAIN_BODY =>
 					i := i + 1;
-					block_index <= conv_std_logic_vector(i, width);
-					si := conv_integer(unsigned(block_outval));
+					perm_index <= conv_std_logic_vector(i, width);
+					si := conv_integer(unsigned(perm_input));
 					cstate := MAIN_BODY_SET_J;
 				
 				when MAIN_BODY_SET_J =>
 					j := j + si;
-					block_index <= conv_std_logic_vector(j, width);
-					sj := conv_integer(unsigned(block_outval));
+					perm_index <= conv_std_logic_vector(j, width);
+					sj := conv_integer(unsigned(perm_input));
 					cstate := MAIN_BODY_SWAP_SI;
 
 				when MAIN_BODY_SWAP_SI =>
-					block_index <= conv_std_logic_vector(i, width);
-					block_inval <= conv_std_logic_vector(sj, width);
-					block_set <= '1';
+					perm_ctrl <= '0';
+					perm_index <= conv_std_logic_vector(i, width);
+					perm_output <= conv_std_logic_vector(sj, width);
+					perm_ctrl <= '1';
 					cstate := MAIN_BODY_SWAP_SJ;
 				
 				when MAIN_BODY_SWAP_SJ =>
-					block_set <= '0';
-					block_index <= conv_std_logic_vector(j, width);
-					block_inval <= conv_std_logic_vector(si, width);
-					block_set <= '1';
+					perm_ctrl <= '0';
+					perm_index <= conv_std_logic_vector(j, width);
+					perm_output <= conv_std_logic_vector(si, width);
+					perm_ctrl <= '1';
 					cstate := MAIN_BODY_PREPARE_OUTPUT;
 
 				when MAIN_BODY_PREPARE_OUTPUT =>
+					perm_ctrl <= '0';
 					tmp := (si + sj) mod 256;
-					block_set <= '0';
-					block_index <= conv_std_logic_vector(tmp, width);
-					sm := conv_integer(unsigned(block_outval));
+					perm_index <= conv_std_logic_vector(tmp, width);
+					sm := conv_integer(unsigned(perm_input));
 					cstate := MAIN_BODY_OUTPUT;
 					
 				when MAIN_BODY_OUTPUT =>
-					output <= input xor conv_std_logic_vector(sm, width);
+					enc_output <= enc_input xor conv_std_logic_vector(sm, width);
 					rdy <= '1';
 					cstate := WHILE_0_RET;
 
 				when WHILE_0_RET =>
-					cstate := WHILE_0_TEST;
+					cstate := WHILE_GO_TEST;
 
 				when others =>
-					output <= "11111111";
+					enc_output <= "11111111";
 
 			end case;
 		end if;
 	end process;
 end Behavioral;
-
---
+++ src/rc4_crypto_tb.vhd
@@ -0,0 +1,120 @@
+LIBRARY ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+ENTITY rc4_crypto_tb IS
+END rc4_crypto_tb;
+ 
+ARCHITECTURE behavior OF rc4_crypto_tb IS 
+    -- Component Declaration for the Unit Under Test (UUT) 
+    COMPONENT rc4_crypto
+    PORT(
+         enc_input : IN  std_logic_vector(7 downto 0);
+         perm_input : IN  std_logic_vector(7 downto 0);
+         go : IN  std_logic;
+         clk : IN  std_logic;
+         enc_output : OUT  std_logic_vector(7 downto 0);
+         perm_ctrl : OUT  std_logic;
+         perm_index : OUT  std_logic_vector(7 downto 0);
+         perm_output : OUT  std_logic_vector(7 downto 0);
+         rdy : OUT  std_logic
+        );
+    END COMPONENT;
+    
+    --Inputs
+    signal enc_input : std_logic_vector(7 downto 0) := (others => '0');
+    signal perm_input : std_logic_vector(7 downto 0) := (others => '0');
+    signal go : std_logic := '0';
+    signal clk : std_logic := '0';
+
+  	--Outputs
+    signal enc_output : std_logic_vector(7 downto 0);
+    signal perm_ctrl : std_logic;
+    signal perm_index : std_logic_vector(7 downto 0);
+    signal perm_output : std_logic_vector(7 downto 0);
+    signal rdy : std_logic;
+
+    -- Clock period definitions
+    constant clk_period : time := 10 ns;
+	
+	subtype rc4int is integer range 0 to 255;
+	type my_array is array (0 to 255) of rc4int;
+ 
+	shared variable sarr : my_array := (
+				000, 185, 126, 115, 175, 200, 169, 108,
+				008, 013, 041, 091, 189, 046, 116, 109,
+				016, 120, 020, 078, 049, 012, 038, 213,
+				024, 096, 094, 001, 178, 206, 067, 105,
+				032, 156, 055, 158, 073, 081, 145, 009,
+				040, 002, 050, 039, 172, 244, 243, 139,
+				048, 040, 201, 063, 164, 165, 207, 170,
+				056, 159, 118, 061, 010, 222, 247, 104,
+				064, 223, 087, 193, 110, 099, 071, 031,
+				072, 203, 135, 034, 015, 161, 174, 029,
+				080, 019, 103, 080, 162, 056, 154, 058,
+				088, 234, 209, 236, 023, 151, 051, 060,
+				096, 090, 176, 113, 121, 230, 212, 251,
+				104, 026, 245, 097, 003, 035, 191, 238,
+				112, 249, 181, 188, 192, 205, 182, 027,
+				120, 184, 195, 119, 028, 112, 235, 079,
+				128, 086, 018, 171, 198, 007, 130, 043,
+				136, 092, 076, 025, 147, 054, 150, 014,
+				144, 030, 211, 084, 229, 037, 237, 000,
+				152, 044, 157, 083, 246, 088, 137, 253,
+				160, 075, 069, 017, 057, 047, 036, 059,
+				168, 242, 006, 153, 129, 004, 052, 202,
+				176, 085, 144, 106, 177, 190, 117, 187,
+				184, 204, 070, 226, 194, 186, 127, 033,
+				192, 136, 024, 100, 124, 180, 095, 173,
+				200, 239, 072, 005, 219, 066, 149, 228,
+				208, 210, 141, 143, 082, 208, 217, 215,
+				216, 053, 125, 021, 131, 214, 231, 022,
+				224, 074, 224, 252, 102, 107, 221, 077,
+				232, 140, 068, 062, 248, 255, 233, 227,
+				240, 114, 016, 065, 160, 111, 101, 196,
+				248, 197, 032, 183, 152, 216, 241, 011);
+BEGIN
+	-- Instantiate the Unit Under Test (UUT)
+	uut: rc4_crypto PORT MAP (
+		enc_input => enc_input,
+        perm_input => perm_input,
+        go => go,
+		clk => clk,
+        enc_output => enc_output,
+        perm_ctrl => perm_ctrl,
+        perm_index => perm_index,
+        perm_output => perm_output,
+        rdy => rdy
+    );
+
+    -- Clock process definitions
+    clk_process :process
+    begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+    end process;
+	
+	array_proc: process(clk, perm_ctrl, perm_index, perm_output)
+	begin
+		if rising_edge(clk) then
+			if perm_ctrl = '1' then
+				sarr(conv_integer(unsigned(perm_index))) := conv_integer(unsigned(perm_output));
+			else
+				perm_input <= conv_std_logic_vector(sarr(conv_integer(unsigned(perm_index))), 8);
+			end if;
+		end if;
+	end process;
+ 
+   -- Stimulus process
+	stim_proc: process
+	begin		
+		-- hold reset state for 100 ns.
+		wait for 100 ns;	
+
+		go <= '1';
+
+		wait;
+	end process;
+END;

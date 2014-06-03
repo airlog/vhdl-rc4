b1c90153811970acbfd05d2ec41a7a587c2fff8a
robin92 <robin92pl@gmail.com>
2014-06-03 12:27:32 +0200

    Zaimplementowano urządzenie szyfrująco-deszyfrujące strumień RC4.
    Zaktualizowano próbną tablicę stanu RC4 po inicjalizacji kluczem 0xFAEBDC0019283746
    w testbench.
    Do procesu rc4_crypto wprowadzono zmienną clk_ctr umożliwiającą zliczanie cykli.
    Stosujemy to do utrzymania sygnałów przez określony czas.

--- src/rc4_crypto.vhd
+++ src/rc4_crypto.vhd
@@ -22,8 +22,7 @@ end rc4_crypto;
 architecture Behavioral of rc4_crypto is
 	type rc4_crypto_state is (
 			WHILE_GO_TEST, WHILE_GO_RET,
-			MAIN_BODY, MAIN_BODY_SET_J, MAIN_BODY_SWAP_SI, MAIN_BODY_SWAP_SJ,
-			MAIN_BODY_PREPARE_OUTPUT, MAIN_BODY_OUTPUT,
+			MAIN_BODY, MAIN_BODY_OUTPUT,
 			WHILE_0_RET
 		);
 	subtype rc4int is integer range 0 to 255;
@@ -40,48 +39,83 @@ begin
 			case cstate is
 				when WHILE_GO_TEST =>
 					if go = '1' then
-						enc_output <= "11000000";
 						cstate := MAIN_BODY;
-					else
-						enc_output <= "00000000";
 					end if;
 					
 				when WHILE_GO_RET =>
 					cstate := WHILE_GO_TEST;
 				
 				when MAIN_BODY =>
-					i := i + 1;
-					perm_index <= conv_std_logic_vector(i, width);
-					si := conv_integer(unsigned(perm_input));
-					cstate := MAIN_BODY_SET_J;
+					case clk_ctr is
+						when 0 =>
+							clk_ctr := 0;
+							i := i + 1;
+							perm_index <= conv_std_logic_vector(i, width);
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						-- utrzymaj stan sygna³u perm_index ¿eby otrzymaæ poprawn¹ wartoœæ
+						when 1 =>
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						when 2 =>
+							si := conv_integer(unsigned(perm_input));
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						when 3 =>
+							j := j + si;
+							perm_index <= conv_std_logic_vector(j, width);
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						-- utrzymaj stan sygna³u perm_index ¿eby otrzymaæ poprawn¹ wartoœæ
+						when 4 =>
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						when 5 =>
+							sj := conv_integer(unsigned(perm_input));
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						when 6 =>
+							perm_ctrl <= '0';
+							perm_index <= conv_std_logic_vector(i, width);
+							perm_output <= conv_std_logic_vector(sj, width);
+							perm_ctrl <= '1';
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+							
+						when 7 =>
+							perm_ctrl <= '0';
+							perm_index <= conv_std_logic_vector(j, width);
+							perm_output <= conv_std_logic_vector(si, width);
+							perm_ctrl <= '1';
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+							
+						when 8 =>
+							perm_ctrl <= '0';
+							tmp := (si + sj) mod 256;
+							perm_index <= conv_std_logic_vector(tmp, width);
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						-- utrzymaj stan sygna³u perm_index ¿eby otrzymaæ poprawn¹ wartoœæ
+						when 9 =>
+							clk_ctr := clk_ctr + 1;
+							cstate := MAIN_BODY;
+						
+						when 10 =>
+							sm := conv_integer(unsigned(perm_input));
+							clk_ctr := 0;
+							cstate := MAIN_BODY_OUTPUT;
+							
+						when others =>
+					end case;					
 				
-				when MAIN_BODY_SET_J =>
-					j := j + si;
-					perm_index <= conv_std_logic_vector(j, width);
-					sj := conv_integer(unsigned(perm_input));
-					cstate := MAIN_BODY_SWAP_SI;
-
-				when MAIN_BODY_SWAP_SI =>
-					perm_ctrl <= '0';
-					perm_index <= conv_std_logic_vector(i, width);
-					perm_output <= conv_std_logic_vector(sj, width);
-					perm_ctrl <= '1';
-					cstate := MAIN_BODY_SWAP_SJ;
-				
-				when MAIN_BODY_SWAP_SJ =>
-					perm_ctrl <= '0';
-					perm_index <= conv_std_logic_vector(j, width);
-					perm_output <= conv_std_logic_vector(si, width);
-					perm_ctrl <= '1';
-					cstate := MAIN_BODY_PREPARE_OUTPUT;
-
-				when MAIN_BODY_PREPARE_OUTPUT =>
-					perm_ctrl <= '0';
-					tmp := (si + sj) mod 256;
-					perm_index <= conv_std_logic_vector(tmp, width);
-					sm := conv_integer(unsigned(perm_input));
-					cstate := MAIN_BODY_OUTPUT;
-					
 				when MAIN_BODY_OUTPUT =>
 					enc_output <= enc_input xor conv_std_logic_vector(sm, width);
 					rdy <= '1';
@@ -89,9 +123,6 @@ begin
 
 				when WHILE_0_RET =>
 					cstate := WHILE_GO_TEST;
-
-				when others =>
-					enc_output <= "11111111";
 
 			end case;
 		end if;
